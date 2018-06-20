6bb39b5f6d3e1873c2d08b504d54116e6a3277b6
JohnAsare <johnasare7@gmail.com>
2018-06-19 23:04:53 -0400

    Wrote a small code to calculate the angel between clock hands (the hour and the minutes)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "hdfdf";
-    std::cout << "I finally learned how to commit using the Clion IDE";
+/* Formula to calculate the angel between the hour hand and the minutes hand is |30*h - 5.5x| where x is the mins
+ * and where h is hours.
+ *
+ * Brief knowledge: The minutes hand moves 360 degree so it moves between the each numbers, it moves 6 degree.
+ * while the minutes hand moves, the hour hand moves as well. So it moves 30 degree in an hour so that will be
+ * 0.5 degree*/
+
+int calcAngel(double h, double m){
+
+    if (h < 0 || m < 0 || h > 12 || m > 60){
+        printf("wrong time, please check how you typed the time.");
+
+    }
+    int angel= (30*h - 5.5*m);
+
+    return angel;
+}
+
+int main(){
+
+    printf("%d", calcAngel(12, 00));
     return 0;
 }
